71ac39a348052b3281d137d230a7637d5a4db632
Julien M <julien.mifsud77@gmail.com>
2018-02-26 15:38:55 +0100

    Delete user

--- crud/routes/users.js
+++ crud/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+
+/* Delete user */
+router.get("/delete/:id", function(req, res) {
+  User.findByIdAndRemove(req.params.id, function(err, item){
+      if(err)
+          return res.send("Error ! ");
+      
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
